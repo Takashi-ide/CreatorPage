ef02d915332469047148f2f1d2ac16a06d8782e7
takashi-ide <rpjdpjdpjd0408@gmail.com>
2018-08-14 09:56:17 +0900

    views and templetes, not urls

--- create/views.py
+++ create/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import TourInfo
+from .models import SpotInfo
 
 # Create your views here.
 def post_list(request):
     posts = TourInfo.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'create/post_list.html', {'posts':posts})
+
+def account(request):
+
+    return render(request, 'create/account.html', {})
+
+def accountedit(request):
+
+    return render(request, 'create/accountedit.html', {})
+
+    
