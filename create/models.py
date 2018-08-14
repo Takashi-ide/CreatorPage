9b84e4b22f7433925a63cbf53a39696b5fee55d3
takashi-ide <rpjdpjdpjd0408@gmail.com>
2018-08-14 09:39:06 +0900

    model

--- create/models.py
+++ create/models.py
@@ -3,14 +3,20 @@ from django.db import models
 from django.utils import timezone
 
 
-class Post(models.Model):
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+class TourInfo(models.Model):
+    tour_id = models.AutoField(primary_key=True)
+    #user_id = models.CharField(max_length=20)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    geo_hash = models.CharField(max_length=10)
     title = models.CharField(max_length=200)
-    text = models.TextField()
+    description = models.TextField()
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
             blank=True, null=True)
+    like = models.DecimalField(max_digits=5, decimal_places=2)
+    time = models.BigIntegerField()
+    money = models.BigIntegerField()
 
     def publish(self):
         self.published_date = timezone.now()
@@ -18,3 +24,12 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class SpotInfo(models.Model):
+    tour_id = models.BigIntegerField()
+    spot_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    latitude = models.DecimalField(max_digits=15, decimal_places=10)
+    longitude = models.DecimalField(max_digits=15, decimal_places=10)
--- create/views.py
+++ create/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.utils import timezone
-from .models import Post
+from .models import TourInfo
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = TourInfo.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'create/post_list.html', {'posts':posts})
