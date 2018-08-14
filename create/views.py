1d58bf278e0e2aeeb12f7db053d2c8f743e969b1
takashi-ide <rpjdpjdpjd0408@gmail.com>
2018-08-15 03:52:19 +0900

    post_list TourInfo sorting completed

--- create/views.py
+++ create/views.py
@@ -5,7 +5,8 @@ from .models import SpotInfo
 
 # Create your views here.
 def post_list(request):
-    posts = TourInfo.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    logged_in_user = request.user
+    posts = TourInfo.objects.filter(user = user, published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'create/post_list.html', {'posts':posts})
 
 def account(request):
@@ -15,5 +16,3 @@ def account(request):
 def accountedit(request):
 
     return render(request, 'create/accountedit.html', {})
-
-    
